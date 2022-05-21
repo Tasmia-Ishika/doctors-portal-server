b3d89471793176548639a463db06dfe92d53f264
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-05-21 13:24:16 +0600

    changes added

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function verifyJWT(req, res, next) {
         if (err) {
             console.log(err)
             return res.status(403).send({ message: 'forbidden access' })
-           
+
         }
         req.decoded = decoded;
         next();
@@ -33,13 +33,26 @@ function verifyJWT(req, res, next) {
 async function run() {
     try {
         await client.connect();
+        console.log('db connected')
         const serviceCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('bookings');
         const userCollection = client.db('doctors_portal').collection('users');
+        const doctorCollection = client.db('doctors_portal').collection('doctors');
 
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden' });
+            }
+        }
         app.get('/service', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).project({ name: 1 });
             const services = await cursor.toArray();
             res.send(services);
 
@@ -50,32 +63,28 @@ async function run() {
             res.send(users);
         })
 
-        app.get('/admin/:email', async(req, res) =>{
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
-            const user = await userCollection.findOne({email: email});
+            const user = await userCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
-            res.send({admin: isAdmin})
-          })
-
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
-            const email = req.params.email;
-            const requester = req.decoded.email;
-            const requesterAccount = await userCollection.findOne({email: requester});
-            if(requesterAccount.role === 'admin'){
-                const filter = { email: email };
-                const updateDoc = {
-                    $set: { role: 'admin' },
-                };
-                const result = await userCollection.updateOne(filter, updateDoc);
-                res.send(result);
-            }
-            else{
-                res.status(403).send({message: 'forbidden'})
-            }
+            res.send({ admin: isAdmin })
         })
+
+        // Problem started
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
+            console.log(email)
             const user = req.body;
             const filter = { email: email };
             const options = { upsert: true };
@@ -83,11 +92,10 @@ async function run() {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ result, token });
-        })
+        });
 
-       
         // warning  
         // This is not the proper way to query after learning more about mongodb. use aggregate lookup, pipeline, match,group,
         app.get('/available', async (req, res) => {
@@ -121,19 +129,15 @@ async function run() {
             res.send(services);
         })
 
-
-
-
         /**
         * API Naming Convention
         * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
         * app.get('/booking/:id') // get a specific booking 
         * app.post('/booking') // add a new booking
         * app.patch('/booking/:id) //
-        * app.put('/booking/:id') // upsert => update(if exist) or insert(if doesn't exist)
+        * app.put('/booking/:id') // upsert ==> update (if exists) or insert (if doesn't exist)
         * app.delete('/booking/:id) //
-        */
-
+       */
         app.get('/booking', verifyJWT, async (req, res) => {
             const patient = req.query.patient;
             const decodedEmail = req.decoded.email;
@@ -157,6 +161,25 @@ async function run() {
             }
             const result = bookingCollection.insertOne(booking);
             return res.send({ success: true, result })
+        });
+
+        app.get('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+            const doctors = await doctorCollection.find().toArray();
+            res.send(doctors);
+        })
+
+        app.post('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorCollection.insertOne(doctor);
+            res.send(result);
+        })
+
+        app.delete('/doctor/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = {email: email};
+            const result = await doctorCollection.deleteOne(filter
+        );
+            res.send(result);
         })
 
     }
@@ -164,12 +187,14 @@ async function run() {
 
     }
 }
+
 run().catch(console.dir);
 
+
 app.get('/', (req, res) => {
-    res.send('Hello From Portal')
+    res.send('Hello From Doctor Uncle!')
 })
 
 app.listen(port, () => {
-    console.log(`Doctors Portal listening on port ${port}`)
+    console.log(`Doctors App listening on port ${port}`)
 })
